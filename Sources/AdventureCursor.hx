0366c46249ccace0dcc3c31184da09f4b415996c
Robert Konrad <robert@ktx-software.com>
2014-01-25 13:05:00 +0100

    Scene tooltip.

--- Sources/AdventureCursor.hx
+++ Sources/AdventureCursor.hx
@@ -7,6 +7,8 @@ import kha.Font;
 import kha.FontStyle;
 import kha.Loader;
 import kha.Painter;
+import kha.Scene;
+import kha.Sprite;
 
 class AdventureCursor extends AnimatedImageCursor {
 	private var font: Font;
@@ -22,10 +24,25 @@ class AdventureCursor extends AnimatedImageCursor {
 		super.render(painter, x, y);
 		var inventoryItem = Inventory.getItemBelowPoint(x, y);
 		if (inventoryItem != null) {
-			painter.setColor(Color.fromBytes(0, 0, 0));
-			painter.fillRect(x - 2, y - 16 - 2, font.stringWidth(inventoryItem.name) + 4, font.getHeight() + 4);
-			painter.setColor(Color.fromBytes(255, 255, 255));
-			painter.drawString(inventoryItem.name, x, y - 16);
+			drawTooltip(painter, inventoryItem.name, x, y - 16);
 		}
+		else {
+			var worldX = x + Scene.the.screenOffsetX;
+			var worldY = y + Scene.the.screenOffsetY;
+			var items = Scene.the.getHeroesBelowPoint(worldX, worldY);
+			if (items.length > 0) {
+				var item: Sprite = items[0];
+				if (Std.is(item, UseableSprite)) {
+					drawTooltip(painter, cast(item, UseableSprite).name, x, y + 32);
+				}
+			}
+		}
+	}
+	
+	private function drawTooltip(painter: Painter, tip: String, x: Int, y: Int): Void {
+		painter.setColor(Color.fromBytes(0, 0, 0));
+		painter.fillRect(x - 2, y - 2, font.stringWidth(tip) + 4, font.getHeight() + 4);
+		painter.setColor(Color.fromBytes(255, 255, 255));
+		painter.drawString(tip, x, y);
 	}
 }
