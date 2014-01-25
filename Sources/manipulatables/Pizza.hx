450acabe710565025928d1a3376077626cf7537c
Robert Konrad <robert@ktx-software.com>
2014-01-25 17:15:00 +0100

    Jummy pizza fix.

--- Sources/manipulatables/Pizza.hx
+++ Sources/manipulatables/Pizza.hx
@@ -5,7 +5,7 @@ import kha.Loader;
 class Pizza extends UseableSprite
 {
 	public function new() {
-		super("Pizza", Loader.the.getImage("Pizza"));
+		super("Pizza", Loader.the.getImage("pizza_pixel"));
 	}
 	
 }
