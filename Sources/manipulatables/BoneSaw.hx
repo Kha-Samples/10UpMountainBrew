53d3295fa3588ffb5ea4b02bf517c066e2702eae
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2014-01-26 14:44:12 +0100

    BoneSaw texts

--- Sources/manipulatables/BoneSaw.hx
+++ Sources/manipulatables/BoneSaw.hx
@@ -1,7 +1,9 @@
 package manipulatables;
 
+import dialogue.Bla;
 import kha.Image;
 import kha.Loader;
+import manipulatables.ManipulatableSprite.OrderType;
 
 // Knochensäge
 class BoneSaw extends UseableSprite
@@ -9,5 +11,21 @@ class BoneSaw extends UseableSprite
 	public function new( px : Int, py : Int) 
 	{
 		super("Bone Saw", Loader.the.getImage("pizza_pixel"), px, py);
+	}
+	
+	override public function executeOrder(order:OrderType) : Void {
+		if (order == OrderType.Take) {
+			var jmpMan = Jumpman.getInstance();
+			if (jmpMan.hasHelmet) {
+				Dialogue.set([new Bla("TakeBoneSaw_Helmet", jmpMan)]);
+				return;
+			} else if (jmpMan.hasSurgicalMask) {
+				Dialogue.set([new Bla("TakeBoneSaw", jmpMan)]);
+			} else {
+				Dialogue.set([new Bla("TakeBoneSaw_NoMask", jmpMan)]);
+				return;
+			}
+		}
+		super.executeOrder(order);
 	}
 }
